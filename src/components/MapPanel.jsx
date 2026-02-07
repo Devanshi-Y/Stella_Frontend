18eb46c1a87388c72b5cf9974129718b614e5628
Devanshi Yadav <devanshiyadav2006@gmail.com>
2026-02-08 01:56:23 +0530

    Update MapPanel.jsx

--- src/components/MapPanel.jsx
+++ src/components/MapPanel.jsx
@@ -1,1 +1,11 @@
+export default function MapPanel({ state }) {
+  return (
+    <div className="bg-gray-900 p-5 rounded-xl">
+      <h2 className="text-cyan-400 mb-3">Drone Position</h2>
+      <p>True: {state.true_position.join(", ")}</p>
+      <p>Estimated: {state.estimated_position.join(", ")}</p>
+      <p>Waypoint: {state.current_waypoint.join(", ")}</p>
+    </div>
+  );
+}
 
