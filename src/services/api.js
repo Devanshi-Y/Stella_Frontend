58bf2fa2deb7d4eab452b6917fc013c3cbecae97
Devanshi Yadav <devanshiyadav2006@gmail.com>
2026-02-08 02:07:55 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,15 +1,20 @@
+import axios from "axios";
 const BASE = "http://localhost:8000";
 
-export const runSimulation = async () => {
-  await fetch(`${BASE}/run-simulation`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ duration: 90.0, dt: 0.1 }),
-  });
-};
+export const runSimulation = () =>
+  axios.post(`${BASE}/run-simulation`, { duration: 90.0, dt: 0.1 });
 
 export const getState = async () => {
-  const res = await fetch(`${BASE}/current-state`);
-  const data = await res.json();
-  return data.state;
+  const res = await axios.get(`${BASE}/current-state`);
+  return res.data.state;
+};
+
+export const getMetrics = async () => {
+  const res = await axios.get(`${BASE}/metrics`);
+  return res.data.metrics;
+};
+
+export const getTrajectory = async () => {
+  const res = await axios.get(`${BASE}/trajectory`);
+  return res.data.trajectory_data;
 };
